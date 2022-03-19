eb627857952d04b7e8f15ae9a7689a17fc55dafe
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2022-03-19 20:52:58 -0300

    feat: creating page home

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,10 @@
-type ButtonProps = {
-  // texto é optativo, por isso tem interrogaçao
-  text ?: string[];
-  //string[] é array de string
-  // Array<string> é array de string
-}
-// para receber todas as propriedades (text é uma propriedade) do botão coloca dentro do entreparentes
-//props é um objeto que tem ButtonProps como o tipo desse objeto é  ButtonProps
+import {ButtonHTMLAttributes} from 'react'
+
+import '../styles/button.scss'
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
 export function Button(props: ButtonProps) {
-  return (
-    // caso a propriedade text nao exista esta adicionando o default
-    <button>{props.text || 'defaul' }</button>
+	return (
+     <button className="button" {...props} />
   )
 }
-
-export default Button
-
-
-
-// import { useState } from "react";
-
-// export function Button() {
-//   const [counter, setCounter] = useState(0)
-
-//   function increment() {
-//     setCounter(counter + 1);
-//     console.log(counter);
-
-//     return (
-//       <button onClick={increment}>
-//         {counter}
-//       </button>
-//     )
-//   }
-// }
---
+++ src/components/Button_exemplo.tsx
@@ -0,0 +1,35 @@
+type XButtonProps = {
+  // texto é optativo, por isso tem interrogaçao
+  text ?: string[];
+  //string[] é array de string
+  // Array<string> é array de string
+}
+// para receber todas as propriedades (text é uma propriedade) do botão coloca dentro do entreparentes
+//props é um objeto que tem ButtonProps como o tipo desse objeto é  ButtonProps
+export function XButton(props: XButtonProps) {
+  return (
+    // caso a propriedade text nao exista esta adicionando o default
+    <button>{props.text || 'defaul' }</button>
+  )
+}
+
+export default XButton
+
+
+
+// import { useState } from "react";
+
+// export function Button() {
+//   const [counter, setCounter] = useState(0)
+
+//   function increment() {
+//     setCounter(counter + 1);
+//     console.log(counter);
+
+//     return (
+//       <button onClick={increment}>
+//         {counter}
+//       </button>
+//     )
+//   }
+// }
