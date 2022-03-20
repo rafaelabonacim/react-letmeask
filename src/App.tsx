47288d19dfd3db3ede79185ab703fb2a9ef88894
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2022-03-19 21:44:44 -0300

    feat: creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { Button } from "./components/Button";
-// import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes   } from "react-router-dom";
 
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
@@ -11,7 +12,13 @@ function App() {
       
 		// </div>
 		// <Home />
-		<NewRoom />
+		// <NewRoom />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />}/>
+				<Route path="/rooms/new" element={<NewRoom/>}/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
