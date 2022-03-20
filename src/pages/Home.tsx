c9e7b0eb13384a136815b0c6ec60fd501fdbff1d
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2022-03-20 13:04:57 -0300

    feat: creating auth with firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
+import {auth, firebase} from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -6,6 +10,21 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function Home() {
+  const navigate = useNavigate();
+
+  
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result)
+
+      navigate('/rooms/new')
+    })
+
+    
+  }
+  
   return (
     <div id="page-auth">
       <aside>
@@ -16,7 +35,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask"/>
-          <button className='create-room'>
+          <button onClick={handleCreateRoom} className='create-room'>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
