d2c3e7c7ef82c5cb5071a60e944368a684a0e099
rafaela.bonacim <rafaela.bonacim@luizalabs.com>
2022-05-04 18:10:58 -0300

    feat: created the config for newrooms

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 function App() {
 	return (
@@ -21,6 +22,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/rooms/new" element={<NewRoom />} />
+					<Route path="/rooms/:id" element={<Room />} />
 				</Routes>
 			</AuthContextProvider>
 		</BrowserRouter>
